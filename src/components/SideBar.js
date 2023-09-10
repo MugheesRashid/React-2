60533d4dae6df8def07af397819e347d7b1057ed
MugheesRashid <512mughees@gmail.com>
2023-09-10 11:39:16 +0500

    First

---
+++ src/components/SideBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class SideBar extends Component {
+
+  render() {
+    return (
+<>
+<aside>
+
+    
+</aside>
+ 
+
+</>
+    )
+  }
+}
+
+export default SideBar
+
